123bc624ef4ae77e1612760cfda4330204c63b5a
marcus <marcushamlin1@gmail.com>
2021-05-08 14:14:33 +0200

    firebase test

--- app.js
+++ app.js
@@ -78,6 +78,23 @@ function googleSignOut() {
 signIn.addEventListener("click", googleSignIn);
 signOut.addEventListener("click", googleSignOut);
 
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    let uid = user.uid;
+    console.log(uid);
+    // User is signed in.
+
+    firebase
+      .database()
+      .ref("/users/" + uid)
+      .push({
+        test: "testing",
+      });
+  } else {
+    // No user is signed in.
+  }
+});
+
 function writeUserData(e) {
   e.preventDefault();
   console.log("added gratitude entry");
