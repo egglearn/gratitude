80fd131a36b9474336e76a8ee4e706ca941f222d
marcus <marcushamlin1@gmail.com>
2021-05-16 15:27:09 +0200

    test

--- graph.js
+++ graph.js
@@ -14,101 +14,17 @@ var firebaseConfig = {
 var provider = new firebase.auth.GoogleAuthProvider();
 firebase.initializeApp(firebaseConfig);
 
-// set the dimensions and margins of the graph
-
-// var user = firebase.auth().currentUser;
-
-// if (user != null) {
-//   var uid = user.uid;
-//   if (uid) {
-//     console.log("true");
-//   }
-//   // User is signed in.
-// } else {
-//   console.log("dam");
-//   // No user is signed in.
-// }
-
 firebase.auth().onAuthStateChanged(function (user) {
   var uid = user.uid;
-  if (user) {
-    // User is signed in.
 
-    firebase.database().ref("users/" + uid);
-
-    console.log("working");
-  } else {
-    // No user is signed in.
-  }
-
-  function reader() {
-    if (user) {
-      firebase
-        .database()
-        .ref("users/" + uid)
-        .on("value", function (snapshot) {
-          snapshot.forEach(function (childSnapshot) {
-            let datas = childSnapshot.val().gratitude;
-            let graphData = [childSnapshot.val()];
-          });
-        });
-
-      var width = 650;
-      var height = 650;
-
-      // append the svg object to the body of the page
-      var svg = d3
-        .select("#mydata")
-        .append("svg")
-        .attr("width", 450)
-        .attr("height", 450);
-
-      // create dummy data -> just one element per circle
-
-      // Initialize the circle: all located at the center of the svg area
-      var node = svg
-        .append("g")
-        .selectAll("circle")
-        .data(graphData)
-        .enter()
-        .append("circle")
-        .attr("r", 25)
-        .attr("cx", width / 2)
-        .attr("cy", height / 2)
-        .style("fill", "#69b3a2")
-        .style("fill-opacity", 0.3)
-        .attr("stroke", "#69a2b2")
-        .style("stroke-width", 4);
-
-      // Features of the forces applied to the nodes:
-      var simulation = d3
-        .forceSimulation()
-        .force(
-          "center",
-          d3
-            .forceCenter()
-            .x(width / 2)
-            .y(height / 2)
-        ) // Attraction to the center of the svg area
-        .force("charge", d3.forceManyBody().strength(0.5)) // Nodes are attracted one each other of value is > 0
-        .force(
-          "collide",
-          d3.forceCollide().strength(0.01).radius(30).iterations(1)
-        ); // Force that avoids circle overlapping
-
-      // Apply these forces to the nodes and update their positions.
-      // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
-      simulation.nodes(graphData).on("tick", function (d) {
-        node
-          .attr("cx", function (d) {
-            return d.x;
-          })
-          .attr("cy", function (d) {
-            return d.y;
-          });
+  firebase
+    .database()
+    .ref("users/" + uid)
+    .on("value", function (snapshot) {
+      snapshot.forEach(function (childSnapshot) {
+        let datas = childSnapshot.val().gratitude;
+        console.log(datas);
+        let graphData = [childSnapshot.val()];
       });
-    }
-  }
-
-  reader();
+    });
 });
