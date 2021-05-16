d69737006fcd5265a40587668c7e31359ba55c6f
marcus <marcushamlin1@gmail.com>
2021-05-16 20:41:00 +0200

    test

--- graph.js
+++ graph.js
@@ -26,61 +26,68 @@ firebase.auth().onAuthStateChanged(function (user) {
         arr = [childSnapshot.val()];
       });
     });
+
+  d3();
 });
 
 //d3
-var width = 300;
-var height = 300;
 
-// append the svg object to the body of the page
-var svg = d3
-  .select("#mydata")
-  .append("svg")
-  .attr("width", 300)
-  .attr("height", 300);
+function d3() {
+  var width = 300;
+  var height = 300;
 
-// create dummy data -> just one element per circle
+  // append the svg object to the body of the page
+  var svg = d3
+    .select("#mydata")
+    .append("svg")
+    .attr("width", 300)
+    .attr("height", 300);
 
-// Initialize the circle: all located at the center of the svg area
-var node = svg
-  .append("g")
-  .selectAll("circle")
-  .data(arr)
-  .enter()
-  .append("circle")
-  .attr("r", 10)
-  .attr("cx", width / 2)
-  .attr("cy", height / 2)
-  .style("fill", "#69b3a2")
-  .style("fill-opacity", 0.3)
-  .attr("stroke", "#69a2b2")
-  .style("stroke-width", 4);
+  // create dummy data -> just one element per circle
 
-// Features of the forces applied to the nodes:
-var simulation = d3
-  .forceSimulation()
-  .force(
-    "center",
-    d3
-      .forceCenter()
-      .x(width / 2)
-      .y(height / 2)
-  ) // Attraction to the center of the svg area
-  .force("charge", d3.forceManyBody().strength(0.5)) // Nodes are attracted one each other of value is > 0
-  .force("collide", d3.forceCollide().strength(0.01).radius(30).iterations(1)); // Force that avoids circle overlapping
+  // Initialize the circle: all located at the center of the svg area
+  var node = svg
+    .append("g")
+    .selectAll("circle")
+    .data(arr)
+    .enter()
+    .append("circle")
+    .attr("r", 10)
+    .attr("cx", width / 2)
+    .attr("cy", height / 2)
+    .style("fill", "#69b3a2")
+    .style("fill-opacity", 0.3)
+    .attr("stroke", "#69a2b2")
+    .style("stroke-width", 4);
 
-// Apply these forces to the nodes and update their positions.
-// Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
-simulation.nodes(arr).on("tick", function (d) {
-  node
-    .attr("cx", function (d) {
-      return d.x;
-    })
-    .attr("cy", function (d) {
-      return d.y;
-    });
-});
+  // Features of the forces applied to the nodes:
+  var simulation = d3
+    .forceSimulation()
+    .force(
+      "center",
+      d3
+        .forceCenter()
+        .x(width / 2)
+        .y(height / 2)
+    ) // Attraction to the center of the svg area
+    .force("charge", d3.forceManyBody().strength(0.5)) // Nodes are attracted one each other of value is > 0
+    .force(
+      "collide",
+      d3.forceCollide().strength(0.01).radius(30).iterations(1)
+    ); // Force that avoids circle overlapping
 
+  // Apply these forces to the nodes and update their positions.
+  // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
+  simulation.nodes(arr).on("tick", function (d) {
+    node
+      .attr("cx", function (d) {
+        return d.x;
+      })
+      .attr("cy", function (d) {
+        return d.y;
+      });
+  });
+}
 //d3
 
 console.log(arr);
