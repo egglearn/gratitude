8b5e48d145cf0d8582064f08cf1a97e5fe96dd6d
marcus <marcushamlin1@gmail.com>
2021-05-18 20:11:14 +0200

    update

--- graph.js
+++ graph.js
@@ -28,7 +28,6 @@ firebase.auth().onAuthStateChanged(function (user) {
         snapshot.forEach(function (childSnapshot) {
           let datas = childSnapshot.val().gratitude;
           graphData = childSnapshot.val();
-          //console.log(graphData);
 
           //d3
 
