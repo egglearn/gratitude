6e1a152706a2047939a0b845f761c0218b8e747a
marcus <marcushamlin1@gmail.com>
2021-05-19 15:15:45 +0200

    testing on front page

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let graphData;
+let graphData = [];
 
 var firebaseConfig = {
   apiKey: "AIzaSyDpJUKeG8a6eNrhXtd9-EsNuH0pVH9Qjhw",
@@ -152,7 +152,67 @@ firebase.auth().onAuthStateChanged(function (user) {
             let p = document.createElement("p");
             p.textContent = datas;
             gratitudeList.appendChild(p);
+            graphData.push({ datas: datas, test: test });
           });
+
+          //d3
+
+          var width = 100;
+          var height = 100;
+
+          // append the svg object to the body of the page
+          var svg = d3
+            .select("#mydata")
+            .append("svg")
+            .attr("width", 100)
+            .attr("height", 100);
+
+          // create dummy data -> just one element per circle
+
+          // Initialize the circle: all located at the center of the svg area
+          var node = svg
+            .append("g")
+            .selectAll("circle")
+            .data(graphData)
+            .enter()
+            .append("circle")
+            .attr("r", 5)
+            .attr("cx", width / 2)
+            .attr("cy", height / 2)
+            .style("fill", "#69b3a2")
+            .style("fill-opacity", 0.3)
+            .attr("stroke", "#69a2b2")
+            .style("stroke-width", 4);
+
+          // Features of the forces applied to the nodes:
+          var simulation = d3
+            .forceSimulation()
+            .force(
+              "center",
+              d3
+                .forceCenter()
+                .x(width / 2)
+                .y(height / 2)
+            ) // Attraction to the center of the svg area
+            .force("charge", d3.forceManyBody().strength(20)) // Nodes are attracted one each other of value is > 0
+            .force(
+              "collide",
+              d3.forceCollide().strength(0.01).radius(30).iterations(1)
+            ); // Force that avoids circle overlapping
+
+          // Apply these forces to the nodes and update their positions.
+          // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
+          simulation.nodes(graphData).on("tick", function (d) {
+            node
+              .attr("cx", function (d) {
+                return d.x;
+              })
+              .attr("cy", function (d) {
+                return d.y;
+              });
+          });
+
+          //d3
         });
     }
   }
